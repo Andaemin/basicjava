fa36af26f658dcbb38b0d52ab9e7dfbda88056bd
andaemin <eoals0216@gmail.com>
2026-03-14 15:17:46 +0900

    add : pratice method

--- src/method.java
+++ src/method.java
@@ -1,5 +1,10 @@
 public class method {
+    public static void hello(){
+        System.out.println("Pratice Java");
+    }
+    public static void main(String[] args) {
+        hello();
+    }
 
-    
 }
 
