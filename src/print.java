782c9876a1ac59c10971ba60767c447ff8431abc
andaemin <eoals0216@gmail.com>
2026-03-14 15:12:58 +0900

    first commit

---
+++ src/print.java
@@ -0,0 +1,9 @@
+public class print {
+    public  static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("println 은 한줄씩");
+
+        System.out.print("그냥 print는 줄 상관 없이 전부");
+        System.out.print("붙어서 출력됨");
+    }
+}
---
+++ src/variable.java
@@ -0,0 +1,21 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 1;
+        boolean b = true;
+        String c = "hi";
+        char d = 'A';
+        double e = 3.14;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        if (e == 3.14){
+            System.out.println("파이");
+        } else {
+            System.out.println("파이 아님");
+        }
+
+
+    }
+}
