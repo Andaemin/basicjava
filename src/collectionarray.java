43d0e73cf4739e4f0673868fd31c14f8e58e23e0
andaemin <eoals0216@gmail.com>
2026-03-25 17:28:02 +0900

    add : ArrayList

---
+++ src/collectionarray.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class  Person4{
+    String name;
+    Person4(String name){
+        this.name=name;
+    }
+}
+
+
+public class collectionarray {
+    public static void main(String[] args) {
+        ArrayList<Integer> list =new ArrayList<>();
+        ArrayList<Person4> list2 =new ArrayList<>();
+        list.add(10);
+        list2.add(new Person4("nayeon"));
+
+        System.out.println(list);
+        System.out.println(list2.get(0).name);
+
+    }
+
+}
