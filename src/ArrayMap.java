c54861feb7b85c0718b5127202629c01c837aa74
andaemin <eoals0216@gmail.com>
2026-03-25 18:52:42 +0900

    add : Hashmap, set

---
+++ src/ArrayMap.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+
+public class ArrayMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        HashSet<Integer> hashMap2 = new HashSet<>();
+
+        hashMap.put("b", 20);
+        hashMap.put("a", 10);
+        hashMap.put("c", 30);
+
+        hashMap2.add(10);
+        hashMap2.add(10);
+        hashMap2.add(30);
+        for(String key : hashMap.keySet()){
+            System.out.println(key + " : " + hashMap.get(key));
+        }
+        System.out.println(hashMap.containsKey("c"));
+        System.out.println(hashMap2);
+    }
+}
